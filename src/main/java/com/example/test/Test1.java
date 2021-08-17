966781b38b8779dc60acb9aa77faf97220c3a32c
Max <max@maxg.dev>
2021-08-17 15:35:09 +0100

    Format.

--- src/main/java/com/example/test/Test1.java
+++ src/main/java/com/example/test/Test1.java
@@ -1,48 +1,43 @@
 package com.example.test;
+
 import java.util.Scanner;
+
 public class Test1 {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         var t = new Test1();
         String password = t.passwordInput();
         t.passwordChecker(password);
     }
-    private String passwordInput()
-    {
+
+    private String passwordInput() {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter password:");
         return reader.nextLine();
     }
+
     private boolean containsNum(String password) {
         boolean containsNumber = false;
-        for (int checkNumber = 0; checkNumber < 10; checkNumber++)
-        {
+        for (int checkNumber = 0; checkNumber < 10; checkNumber++) {
             if (password.contains("" + checkNumber)) {
                 containsNumber = true;
             }
         }
         return containsNumber;
     }
-    public void passwordChecker(String password)
-    {
+
+    public void passwordChecker(String password) {
         String errorMsg = "";
         String successMsg = "Your password is " + password;
-        if (password.equals(password.toUpperCase()))
-        {
+        if (password.equals(password.toUpperCase())) {
             errorMsg = "Must contain a lower case letter.";
-        }
-        else if (password.equals(password.toLowerCase()))
-        {
+        } else if (password.equals(password.toLowerCase())) {
             errorMsg = "Must contain an upper case letter.";
-        }
-        else if (!containsNum(password))
-        {
+        } else if (!containsNum(password)) {
             errorMsg = "Must contain a number.";
-        }
-        else if (password.length() < 8)
-        {
+        } else if (password.length() < 8) {
             errorMsg = "Must be at least 8 characters long.";
         }
-        System.out.println(errorMsg.equals("") ? successMsg :  errorMsg);
+        System.out.println(errorMsg.equals("") ? successMsg : errorMsg);
     }
 }
