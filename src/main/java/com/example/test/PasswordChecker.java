e51530854660b4d6dde38ea0680d2e832dede45a
Max <max@maxg.dev>
2021-08-17 17:22:09 +0100

    Refactor.

--- src/main/java/com/example/test/PasswordChecker.java
+++ src/main/java/com/example/test/PasswordChecker.java
@@ -4,41 +4,45 @@ import java.util.Scanner;
 
 public class PasswordChecker {
 
-    public static void main(String[] args) {
-        var t = new PasswordChecker();
-        String res = t.check();
-        System.out.println(res);
+    public PasswordChecker() {
     }
 
-    private String passwordInput() {
+    public static void main(String[] args) {
+        var pc = new PasswordChecker();
+        pc.getPassword();
+    }
+
+    private String userInput() {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Enter password:");
+        System.out.println("Enter password: ");
         return reader.nextLine();
     }
 
     private boolean containsNum(String password) {
         boolean containsNumber = false;
         for (int checkNumber = 0; checkNumber < 10; checkNumber++) {
-            if (password.contains("" + checkNumber)) {
+            if (password.contains(String.valueOf(checkNumber))) {
                 containsNumber = true;
             }
         }
         return containsNumber;
     }
 
-    public String check() {
-        String password = passwordInput();
-        String errorMsg = "";
-        String successMsg = "Your password is " + password;
-        if (password.equals(password.toUpperCase())) {
-            errorMsg = "Must contain a lower case letter.";
-        } else if (password.equals(password.toLowerCase())) {
-            errorMsg = "Must contain an upper case letter.";
-        } else if (!containsNum(password)) {
-            errorMsg = "Must contain a number.";
-        } else if (password.length() < 8) {
-            errorMsg = "Must be at least 8 characters long.";
+    private void getPassword() {
+        while (true) {
+            String password = userInput();
+            if (password.equals(password.toUpperCase())) {
+                System.out.println("Must contain a lower case letter.");
+            } else if (password.equals(password.toLowerCase())) {
+                System.out.println("Must contain an upper case letter.");
+            } else if (!containsNum(password)) {
+                System.out.println("Must contain a number.");
+            } else if (password.length() < 8) {
+                System.out.println("Must be at least 8 characters long.");
+            } else {
+                System.out.println("Your password is " + password);
+                break;
+            }
         }
-        return errorMsg.equals("") ? successMsg : errorMsg;
     }
 }
